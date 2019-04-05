8f4937889b373a60303f77b766042732d1674f6d
muasmakkode <muasstepa7@gmail.com>
2019-04-05 16:43:03 +0700

    add material design

---
+++ app/src/main/java/dev/dhyto/material_design_component/HomeActivity.java
@@ -0,0 +1,14 @@
+package dev.dhyto.material_design_component;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
